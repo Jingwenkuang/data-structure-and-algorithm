76ce13af28f19bd47ba459ab0c01671d949ca06e
jingwen kuang <jwkwendy@gmail.com>
2021-10-18 01:48:31 -0700

    maxPathSum

---
+++ 29maxRoottoLeafPathSum.js
@@ -0,0 +1,57 @@
+/*
+max root to leaf path sum
+Write a function, maxPathSum, that takes in the root of a binary tree that contains number values. The function should return the maximum sum of any root to leaf path within the tree.
+
+You may assume that the input tree is non-empty.
+
+test_00:
+const a = new Node(3);
+const b = new Node(11);
+const c = new Node(4);
+const d = new Node(4);
+const e = new Node(-2);
+const f = new Node(1);
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//       3
+//    /    \
+//   11     4
+//  / \      \
+// 4   -2     1
+
+maxPathSum(a); // -> 18
+ */
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+//dfs recursive time 0(n), space o(n)
+
+const maxPathSum = (root) => {
+  if (root === null) return -Infinity;
+  if (root.left === null & root.right === null) return root.val;
+  // console.log(root.val, root.left, root.right)
+  return root.val + Math.max(maxPathSum(root.left), maxPathSum(root.right))
+};
+
+const a = new Node(3);
+const b = new Node(11);
+const c = new Node(4);
+const d = new Node(4);
+const e = new Node(-2);
+const f = new Node(1);
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+console.log(maxPathSum(a))
