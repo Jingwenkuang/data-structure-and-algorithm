fa28fa054cd4938f8e5fbe616ebc86df46917d77
jingwen kuang <jwkwendy@gmail.com>
2021-11-02 01:24:04 -0700

    shortestPath

--- 41largestComponent.js
+++ 41largestComponent.js
@@ -14,10 +14,29 @@ largestComponent({
 }); // -> 4
  */
 
+//dfs time o(e), space o(n)
 const largestComponent = (graph) => {
-  
+  let visited = new Set();
+  let largest = 0; 
+
+  for (let node in graph) {
+    console.log('node', node)
+    exploreSize(node, graph, visited)
+  }
 };
 
+const exploreSize = (currentNode, graph, visited) => {
+  if (visited.has(currentNode)) return 0; 
+  visited.add(currentNode);
+  console.log('visited', visited)
+  let size = 1; 
+  for (let neighbor of graph[currentNode]) {
+    console.log('neighbor', neighbor);
+    console.log('size', size)
+    size += exploreSize(neighbor, graph, visited)
+  }
+  return size;
+}
 console.log(largestComponent({
   0: ['8', '1', '5'],
   1: ['0'],
---
+++ 42shortestPath.js
@@ -0,0 +1,28 @@
+/*
+shortest path
+Write a function, shortestPath, that takes in an array of edges for an undirected graph and two nodes (nodeA, nodeB). The function should return the length of the shortest path between A and B. Consider the length as the number of edges in the path, not the number of nodes. If there is no path between A and B, then return -1.
+
+test_00:
+const edges = [
+  ['w', 'x'],
+  ['x', 'y'],
+  ['z', 'y'],
+  ['z', 'v'],
+  ['w', 'v']
+];
+
+shortestPath(edges, 'w', 'z'); // -> 2
+ */
+
+const shortestPath = (edges, nodeA, nodeB) => {
+
+};
+const edges = [
+  ['w', 'x'],
+  ['x', 'y'],
+  ['z', 'y'],
+  ['z', 'v'],
+  ['w', 'v']
+];
+
+console.log(shortestPath(edges, 'w', 'z')); // -> 2
